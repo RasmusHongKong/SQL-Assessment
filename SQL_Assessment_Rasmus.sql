8ed3cf7f230165ff9f226a024d5428af5001ac54
RasmusHongKong <120019919+RasmusHongKong@users.noreply.github.com>
2023-03-09 17:20:25 +0100

    Update SQL_Assessment_Rasmus.sql

--- SQL_Assessment_Rasmus.sql
+++ SQL_Assessment_Rasmus.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE rasmus_sql_assessment;
 USE rasmus_sql_assessment;
 
+# Creating the students table
 CREATE TABLE students (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(100)
 );
 
+# Creating the papers table
 CREATE TABLE papers (
     title VARCHAR(255),
     grade INT,
@@ -19,7 +21,6 @@ INSERT INTO students (first_name) VALUES
 ('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
 
 # Inserting values into Papers
-
 INSERT INTO papers (student_id, title, grade ) VALUES
 (1, 'My First Book Report', 60),
 (1, 'My Second Book Report', 75),
@@ -27,6 +28,9 @@ INSERT INTO papers (student_id, title, grade ) VALUES
 (2, 'De Montaigne and The Art of The Essay', 98),
 (4, 'Borges and Magical Realism', 89);
 
+# Printing the tables from Results_SQL_assessment
+
+# 1: Showing first name, title and grade of papers, sorted by grade
 SELECT 
     a.first_name AS 'First name',
     b.title AS Title,
@@ -37,6 +41,7 @@ FROM
     papers b ON a.id = b.student_id
 ORDER BY b.grade DESC;
 
+# 2: First name, title, grade, showing all students with null values for those who did not submit, default order
 SELECT 
     a.first_name AS 'First name',
     b.title AS Title,
@@ -46,6 +51,7 @@ FROM
         LEFT JOIN
     papers b ON a.id = b.student_id;
 
+# 3: Same as above, but with MISSING and 0 values for no submission
 SELECT 
     a.first_name AS 'First Name',
     COALESCE(b.title, 'MISSING') AS Title,
@@ -55,6 +61,7 @@ FROM
         LEFT JOIN
     papers b ON a.id = b.student_id;
 
+# 4: First name sorted by grade average
 SELECT 
     a.first_name AS 'First name',
     ROUND(COALESCE(AVG(b.grade), 0), 1) AS Average
@@ -65,6 +72,7 @@ FROM
 GROUP BY a.first_name
 ORDER BY Average DESC;
 
+# 5: First name sorted by grade average, with passing/failing
 SELECT 
     a.first_name AS 'First name',
     ROUND(COALESCE(AVG(b.grade), 0), 1) AS Average,
